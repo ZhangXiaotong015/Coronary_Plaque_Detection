c7529c2636f1f1e4870407484e19cba22474a1dc
Xiaotong Zhang <31834504+ZhangXiaotong015@users.noreply.github.com>
2025-12-19 16:32:22 +0100

    Update run.sh

--- Polar/Dockerfile/run.sh
+++ Polar/Dockerfile/run.sh
@@ -7,13 +7,13 @@ mkdir -p "$data_test_polarCT"
 mkdir -p "$data_test_polarCT_25D"
 docker run --rm --gpus "device=0" \
     -v "$HOST_OUTPUT_DIR":/output \
-    -v /mnt/e/xiaotong/WSL/TestData/PlaqueDet/oriCT/deeplearning:/data_test_CT \
+    -v /mnt/e/WSL/TestData/PlaqueDet/oriCT/deeplearning:/data_test_CT \
     -v "$data_test_polarCT":/data_test_polarCT \
     -v "$data_test_polarCT_25D":/data_test_polarCT_25D \
     -e MODEL_PATH=/app/model/40_checkpoint.npz \
     -e SPARSE_MATRIX_PATH=/app/model/lumenCenter_sampleLine_sparseMetrix.npy \
     -e SAMPLE_LINE_PATH=/app/model/lumenCenter_sampleLine.npy \
-    plaque_det_maskrcnn:20251119 \
+    plaque_det:polar \
     --output_dir /output \
     --data_test_CT /data_test_CT \
     --data_test_polarCT /data_test_polarCT \
@@ -31,13 +31,13 @@ docker run --rm --gpus "device=0" \
 # mkdir -p "$data_test_polarCT_25D"
 # docker run --rm -it --gpus "device=0" \
 #     -v "$output":/output \
-#     -v /mnt/e/xiaotong/WSL/TestData/PlaqueDet/oriCT/deeplearning:/data_test_CT \
+#     -v /mnt/e/WSL/TestData/PlaqueDet/oriCT/deeplearning:/data_test_CT \
 #     -v "$data_test_polarCT":/data_test_polarCT \
 #     -v "$data_test_polarCT_25D":/data_test_polarCT_25D \
 #     -e MODEL_PATH=/app/model/40_checkpoint.npz \
 #     -e SPARSE_MATRIX_PATH=/app/model/lumenCenter_sampleLine_sparseMetrix.npy \
 #     -e SAMPLE_LINE_PATH=/app/model/lumenCenter_sampleLine.npy \
-#     plaque_det_maskrcnn:20251119 \
+#     plaque_det:polar \
 #     bash
 
 ##### All mounted paths must be exist!!!
